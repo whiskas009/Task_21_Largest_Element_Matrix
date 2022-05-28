5a7dc2450dc3aa571967ced2d05f62f6e2ec8274
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-28 17:43:11 +0300

    #1

---
+++ Task_21_Largest_Element_Matrix/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_21_Largest_Element_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int minSizeMatrix = 1;
+            int maxSizeMatrix = 10;
+
+            int maxElementMatrix = 0;
+
+            int numberRow = rand.Next(minSizeMatrix, maxSizeMatrix);
+            int numberColumn = rand.Next(minSizeMatrix, maxSizeMatrix);
+
+            int[,] array = new int [numberRow, numberColumn];
+            Console.WriteLine("Исходная матрица: \n");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rand.Next();
+                    if (maxElementMatrix < array[i, j])
+                    {
+                        maxElementMatrix = array[i, j];
+                    }
+                    Console.Write(array[i, j]);
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine($"\nМаксимальный элемент в матрице {maxElementMatrix}\n");
+            Console.WriteLine("Изменённая матрица: \n");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] == maxElementMatrix)
+                    {
+                        array[i, j] = 0;
+                    }
+                    Console.Write(array[i, j]);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
